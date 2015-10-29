b9cb052cfbcf115603428d70a3581231aa101f58
brandly <brandly13@gmail.com>
2015-10-29 15:02:52 -0400

    change test, make it pass

--- test/tests.js
+++ test/tests.js
@@ -5,8 +5,8 @@ const mansplain = require('../');
 
 describe('mansplain', function () {
   it('should convert an npm man link to an html anchor element', function () {
-    const input = '<li>npm-cmd(9)</li>';
+    const input = '<h1>npm-cmd(9)</h1><li>npm-cmd(9)</li>';
     const result = mansplain(input);
-    result.should.equal('<li><a href="cli/cmd">cmd</a></li>');
+    result.should.equal('<h1>npm-cmd</h1><li><a href="cmd">npm-cmd</a></li>');
   });
 });
